e1dd259e4328ccd7866e1431e01a5dc485214991
TrullMiester <myang2@cps.edu>
2022-07-06 16:41:21 +0000

    distsensor

--- motion.py
+++ motion.py
@@ -8,8 +8,10 @@ import time
 # stop function.
 
 def fwd(bot):
-  GOAL = 250 
-  TICKS = 1
+  servo = bot.init_servo()
+  servo.reset_servo()
+  GOAL = 200
+  TICKS = 4
   LOOP_GOAL = GOAL / TICKS
   sum_error = 0
   
@@ -17,32 +19,43 @@ def fwd(bot):
   kI = 0
   kD = 0
 
-  left_speed = 250 
-  right_speed = 250
+  left_speed =  GOAL
+  right_speed = GOAL
 
   bot.reset_encoders(True)
   bot.set_motor_dps(bot.MOTOR_LEFT, left_speed)
   bot.set_motor_dps(bot.MOTOR_RIGHT, right_speed)
 
-  end = time.time()+25
+  sensor = bot.init_distance_sensor()
+
+  end = time.time() + 60
   while time.time() < end:
-    time.sleep(1)
+    time.sleep(0.25)
     left_position, right_position = bot.read_encoders()
     print(left_position, right_position)
-    bot.reset_encoders(True)
     
     left_error = LOOP_GOAL - left_position
     right_error = LOOP_GOAL - right_position
     
     left_speed += kP * left_error
-    right_speed += kP * right_error * 2
+    
+    if right_error > 0:
+        right_error *= 1.05
+
+    right_speed += kP * right_error
 
     print(left_speed, right_speed)
     
+    if sensor.read_mm() < 300: 
+        bot.orbit(90)
+
+    bot.reset_encoders(True)
     bot.set_motor_dps(bot.MOTOR_LEFT, left_speed)
     bot.set_motor_dps(bot.MOTOR_RIGHT, right_speed)
-
   
-  bot.stop()
-    
-    
+  bot.stop() 
+
+if __name__ == '__main__':
+    bot = easygopigo3.EasyGoPiGo3() 
+
+    fwd(bot)
