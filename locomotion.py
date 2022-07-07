893ca6f733ed4fa563f889f8951575dc0bf94a87
TrullMiester <64874574+TrullMiester@users.noreply.github.com>
2022-07-07 11:23:26 -0500

    Update locomotion.py

--- locomotion.py
+++ locomotion.py
@@ -1,5 +1,6 @@
 import motion
 import easygopigo3
+import math
 
 def main():
     bot = easygopigo3.EasyGoPiGo3()
@@ -9,10 +10,21 @@ def main():
 
     distance_sensor = bot.init_distance_sensor()
 
-    goal, left_speed, right_speed = 1000, 1000, 1000 #starting speed
-    while distance_sensor.read_mm() > 300:
-        left_speed, right_speed = motion.fwd(bot, goal, left_speed, right_speed)
     
+    orientation = 0 #degrees, straight forward is 0
+    x_distance = 0
+    y_distance = 0 
+    total_distance = 0
+    
+    ORBIT_DIAMETER = 115
+    WHEEL_DIAMETER = 65
+    
+    goal, left_speed, right_speed = 1000, 1000, 1000 #starting speed
+    
+    while distance_sensor.read_mm() > 300:
+        left_speed, right_speed, left_position, right_position = motion.fwd(bot, goal, left_speed, right_speed)
+        
+        
     bot.stop()
 
 if __name__ == '__main__':
