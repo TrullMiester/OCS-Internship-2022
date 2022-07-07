275504a1b058fa4e5480f83773ee51d46a56b956
TrullMiester <myang2@cps.edu>
2022-07-07 13:58:02 +0000

    updated

--- locomotion.py
+++ locomotion.py
@@ -3,7 +3,17 @@ import easygopigo3
 
 def main():
     bot = easygopigo3.EasyGoPiGo3()
-    motion.fwd(bot)
+    
+    servo = bot.init_servo()
+    servo.reset_servo()
+
+    distance_sensor = bot.init_distance_sensor()
+
+    left_speed, right_speed = 200, 200 #starting speed
+    while distance_sensor.read_mm() > 300:
+        left_speed, right_speed = motion.fwd(bot, 200, left_speed, right_speed)
+    
+    bot.stop()
 
 if __name__ == '__main__':
     main()
